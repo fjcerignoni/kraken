7e98238f9f06a2e664e2b0e2aa242f30ae7cbfc8
Felipe Cerignoni <fcerignoni@gmail.com>
2023-03-08 23:23:26 -0300

    solved god_id bug and query adjustments

--- bot/cogs/admin.py
+++ bot/cogs/admin.py
@@ -6,9 +6,9 @@ from helpers import get_guild, get_guild_players
 ## TODO Adicionar logging
 
 class Admin(commands.Cog):
-    def __init__(self, bot:commands.Bot, god_id:int, Session) -> None:
+    def __init__(self, bot:commands.Bot, god_id:str, Session) -> None:
         self.bot = bot
-        self.god_id = god_id
+        self.god_id = int(god_id)
         self.Session = Session
 
     @commands.command()
@@ -25,6 +25,10 @@ class Admin(commands.Cog):
                         server = session.query(Servers) \
                                 .filter(Servers.server_id == ctx.guild.id) \
                                 .one_or_none()
+                        
+                        guild = session.query(AlbionGuilds) \
+                                .filter(AlbionGuilds.server_id == ctx.guild.id) \
+                                .one_or_none()
 
                         # if server not exists then create record in the database
                         if server is None:
@@ -39,55 +43,57 @@ class Admin(commands.Cog):
 
                         else:
                             await ctx.send(f"{ctx.guild.name} já foi iniciado.")
+
+                    if guild is None:
                     
-                    # ask the guild name at albion online
-                    await ctx.send("Qual o nome da sua guild no albion?")
-                   
-                    # wait for user to respond    
-                    message = await self.bot.wait_for('message', check= lambda message: 
-                                                        message.author == ctx.guild.owner or \
-                                                        message.author.id == self.god_id \
-                                                     )
-
-                    # fetch guild in tools4albion API based on user response
-                    albion_guild = await get_guild(message.content)
-                    if albion_guild is None:
-                        await ctx.send("Guild não encontrada.")
-                        return
-
-                    # ask if guild name provide by the API is correct 
-                    msg = await ctx.send(f"O nome da sua guild é exatamente: {albion_guild.Name} ?")
+                        # ask the guild name at albion online
+                        await ctx.send("Qual o nome da sua guild no albion?")
                     
-                    await msg.add_reaction("\U0001F44D")
-                    await msg.add_reaction("\U0001F44E")
+                        # wait for user to respond    
+                        message = await self.bot.wait_for('message', check= lambda message: 
+                                                            message.author == ctx.guild.owner or \
+                                                            message.author.id == self.god_id \
+                                                        )
 
-                    # wait for user to react
-                    
-                    reaction, user = await self.bot.wait_for('reaction_add', check= lambda reaction, user:
-                                                                user == ctx.guild.owner or user.id == self.god_id and \
-                                                                str(reaction.emoji) in ["\U0001F44D","\U0001F44E"] \
-                                                            )
-                    
-                    if str(reaction.emoji) == "\U0001F44D": 
-                        # check if guild already exists in the database
-                        with self.Session() as session:
-                            db_guild = session.query(AlbionGuilds) \
-                                        .filter(AlbionGuilds.guild_id == albion_guild.Id) \
-                                        .one_or_none()
+                        # fetch guild in tools4albion API based on user response
+                        albion_guild = await get_guild(message.content)
+                        if albion_guild is None:
+                            await ctx.send("Guild não encontrada.")
+                            return
 
-                            # if not exists then create record in the database
-                            if db_guild is None:
-                                new_guild = AlbionGuilds(guild_id = albion_guild.Id, \
-                                                        guild_name = albion_guild.Name, \
-                                                        server_id = ctx.guild.id)
-                                session.add(new_guild)
-                                session.commit()
+                        # ask if guild name provide by the API is correct 
+                        msg = await ctx.send(f"O nome da sua guild é exatamente: {albion_guild.Name} ?")
+                        
+                        await msg.add_reaction("\U0001F44D")
+                        await msg.add_reaction("\U0001F44E")
 
-                                await ctx.send("Guild cadastrada com sucesso.")
-                            else:
-                                await ctx.send(f'A {db_guild.guild_name} já foi cadastrada por outro servidor')
-                    else:
-                        await ctx.send(f"Operação cancelada.")
+                        # wait for user to react
+                        
+                        reaction, user = await self.bot.wait_for('reaction_add', check= lambda reaction, user:
+                                                                    user == ctx.guild.owner or user.id == self.god_id and \
+                                                                    str(reaction.emoji) in ["\U0001F44D","\U0001F44E"] \
+                                                                )
+                        
+                        if str(reaction.emoji) == "\U0001F44D": 
+                            # check if guild already exists in the database
+                            with self.Session() as session:
+                                db_guild = session.query(AlbionGuilds) \
+                                            .filter(AlbionGuilds.guild_id == albion_guild.Id) \
+                                            .one_or_none()
+
+                                # if not exists then create record in the database
+                                if db_guild is None:
+                                    new_guild = AlbionGuilds(guild_id = albion_guild.Id, \
+                                                            guild_name = albion_guild.Name, \
+                                                            server_id = ctx.guild.id)
+                                    session.add(new_guild)
+                                    session.commit()
+
+                                    await ctx.send("Guild cadastrada com sucesso.")
+                                else:
+                                    await ctx.send(f'A {db_guild.guild_name} já foi cadastrada por outro servidor')
+                        else:
+                            await ctx.send(f"Operação cancelada.")
 
                 except Exception as e:
                     await ctx.send("Ocorreu um erro.")
@@ -96,89 +102,25 @@ class Admin(commands.Cog):
                 await ctx.send("Você não tem permissões para executar esse comando")
 
     @commands.command()
-    async def check_players(self, ctx:commands.Context) -> None:
-        async with ctx.typing():
-            try:
-            # fectch guild info in the database
-                with self.Session() as session:
-                    guild = session.query(AlbionGuilds) \
-                            .filter(AlbionGuilds.server_id == ctx.guild.id) \
-                            .one_or_none()
-
-                # create list with players name consulting tools4albion API and sort
-                players = [player.Name for player in await get_guild_players(guild.guild_id)]
-                players.sort()
-
-                # create chunks to better display the information in discord embed
-                chunk_size= int(len(players) / 3)
-                chunk=[players[i:i + chunk_size] for i in range(0, len(players), chunk_size)]
-
-                column1 = ''.join(f'{player}\n' for player in chunk[0])
-                column2 = ''.join(f'{player}\n' for player in chunk[1])
-                column3 = ''.join(f'{player}\n' for player in chunk[2])
-
-                # crate embed
-                embed = Embed(
-                    title=f'Jogadores da {guild.guild_name}',
-                    description=f'Atualmente temos {len(players)} ativos na guild.'
-                )
-                
-                embed.add_field(name="Jogadores", value=column1, inline=True)
-                embed.add_field(name="Jogadores", value=column2, inline=True)
-                embed.add_field(name="Jogadores", value=column3, inline=True)
-                embed.set_footer(text="""
-Esta lista é carregada do projeto tools4albion e pode
-ter um delay de 2 dias em relação ao Albion Online.
-Desenvolvido por Ac1dTrip""")
-                
-                await ctx.send(embed=embed)
-            except Exception as e:
-                await ctx.send('Ocorreu um erro.')
-                print(e)
-
-
-    ## checar o match entre os membros da guild e os players no albion 
-    #  issue: remover as tags de cargo do member.nick, ex: ['[SM] Ac1dTrip', '[Co-Líder] MagoMM', '[ragedog] Opuc', 'Kraken']
-    # '[SM]🗡 SparttanZ🛡'  / 'SparttanZ'
-    # TODO regex para encontrar o agrupamento de caracteres alfanumericos que não estão entre colchetes [] 
-    #         
-    # @commands.command()
-    # async def check_active_players(self, ctx):
-    #     try:
-    #     # fectch guild info in the database
-    #         with self.Session() as session:
-    #             guild = session.query(AlbionGuilds) \
-    #                     .filter(AlbionGuilds.server_id == ctx.guild.id) \
-    #                     .one_or_none()
-
-    #         # create list with players name consulting tools4albion API and sort
-    #         players = [player.Name for player in await get_guild_players(guild.guild_id)]
-    #         players.sort()
-    #         print(players)
-
-    #         members = [member.nick for member in ctx.guild.members if not member.nick is None]
-        
-    #         members.sort()
-
-    #         print(members)
-        
-    #     except Exception as e:
-    #         await ctx.send('Ocorreu um erro.')
-    #         print(e)
-
-    @commands.command()
     async def give_permition(self, ctx: commands.Context, member: Member, role:str = 'moderator') -> None:
         if ctx.author == ctx.guild.owner or ctx.author.id == self.god_id: 
             async with ctx.typing():
                 try:
                     with self.Session() as session:
-                        db_role = session.query(Roles).filter(Roles.role_name == role).one_or_none()
+                        db_role = session.query(Roles) \
+                                   .filter(Roles.role_name == role) \
+                                   .one_or_none()
                         if db_role is None:
                             await ctx.send(f'{role} não cadastrada.')
                         else:
-                            permited = session.query(Permited).filter(Permited.member_id == member.id).one_or_none()
+                            permited = session.query(Servers, Permited) \
+                                        .join(Permited) \
+                                        .filter(Permited.member_id == member.id) \
+                                        .filter(Servers.server_id == ctx.guild.id) \
+                                        .one_or_none()
+
                             if permited is None:
-                                new_permited = Permited(
+                                new_permited = Permited( \
                                                 member_id = member.id, \
                                                 member_name = member.name, \
                                                 server_id = ctx.guild.id, \
@@ -188,6 +130,33 @@ Desenvolvido por Ac1dTrip""")
                                 await ctx.send(f'Permissão concedida para {member}')
                             else:
                                 await ctx.send(f'{member} ja cadastrado.')
+
+                except Exception as e:
+                    await ctx.send('Ocorreu um erro.')
+                    print(e)
+        else:
+            await ctx.send("Você não tem permissões para executar esse comando")
+
+
+    @commands.command()
+    async def revoke_permition(self, ctx: commands.Context, member: Member) -> None:
+        if ctx.author == ctx.guild.owner or ctx.author.id == self.god_id: 
+            async with ctx.typing():
+                try:
+                    with self.Session() as session:
+                        
+                        permited = session.query(Servers, Permited) \
+                                    .join(Permited) \
+                                    .filter(Permited.member_id == member.id) \
+                                    .filter(Servers.server_id == ctx.guild.id) \
+                                    .one_or_none()
+
+                        if permited is not None:
+                            session.delete(permited.Permited)
+                            session.commit()
+                            await ctx.send(f'Permissão removida para {member}')
+                        else:
+                            await ctx.send(f'{member} não tem permissões.')
 
                 except Exception as e:
                     await ctx.send('Ocorreu um erro.')
