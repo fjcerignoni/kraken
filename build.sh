7c2f94b32159c280952e73ce7d9cf5253d7f4ef9
Felipe Cerignoni <fcerignoni@gmail.com>
2024-08-07 15:18:39 -0300

    adjusted volume variable

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 CONTAINER_NAME=kraken
 IMAGE_NAME=$CONTAINER_NAME-image
+VLOLUME_NAME=$CONTAINER_NAME-db
 
 docker volume create --name $VOLUME_NAME
 
@@ -15,6 +16,6 @@ docker run \
     --detach \
     --env-file=.env \
     --name $CONTAINER_NAME \
-    --volume kraken-db:/opt/kraken/bot/db \
+    --volume $VOLUME_NAME:/opt/kraken/bot/db \
     --restart unless-stopped \
     $IMAGE_NAME
